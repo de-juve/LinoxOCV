5e721bba39507e433e5b695407ab28245ad276b1
de-juve <dmbachurin@gmail.com>
2013-11-24 20:44:34 +0600

    ordinary squared least optimizer

--- src/plugins/TestPlugin.java
+++ src/plugins/TestPlugin.java
@@ -6,7 +6,7 @@ import entities.Point;
 import gui.Linox;
 import org.opencv.core.Mat;
 import plugins.approximation.Interpolacion;
-import plugins.approximation.OLSSimpleRegression;
+import plugins.approximation.Optimizer;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -24,106 +24,106 @@ public class TestPlugin extends AbstractPlugin {
 
 
         b = g = r = 0;
-        result = Mat.zeros( image.size(), image.type() );
+        result = Mat.zeros(image.size(), image.type());
         Random rand = new Random();
-        Linox.getInstance().getStatusBar().setProgress( title, 0, 100 );
+        Linox.getInstance().getStatusBar().setProgress(title, 0, 100);
         HashMap<Integer, Point> wpoints = DataCollector.INSTANCE.getWatershedPoints();
-        LineCreator lineCreator = new LineCreator( DataCollector.INSTANCE.getWatershedImg(), new ArrayList<>( wpoints.values() ) );
+        LineCreator lineCreator = new LineCreator(DataCollector.INSTANCE.getWatershedImg(), new ArrayList<>(wpoints.values()));
         lineCreator.extractEdgePoints();
         lineCreator.createLines();
         ArrayList<Line> lines = lineCreator.lines;
         ArrayList<Point> epoints = lineCreator.edgePoints;
 
-        //interpolacion
         Interpolacion interpolacion = new Interpolacion();
 
-        //
-        OLSSimpleRegression regression = new OLSSimpleRegression();
+        //OLSSimpleRegression regression = new OLSSimpleRegression();
 
-        Mat img = Mat.zeros( image.size(), image.type() );
-        Mat im_inter = Mat.zeros( image.size(), image.type() );
-        Mat im_regr = Mat.zeros( image.size(), image.type() );
+        Optimizer optimizer = new Optimizer();
 
-        for ( Line line : lines ) {
-            if ( line.points.size() <= 2 )
+        Mat img = Mat.zeros(image.size(), image.type());
+        Mat im_inter = Mat.zeros(image.size(), image.type());
+        Mat im_regr = Mat.zeros(image.size(), image.type());
+
+        for (Line line : lines) {
+            if (line.points.size() <= 2)
                 continue;
-            double[] mcolor = new double[]{ b, g, r };
-            r = rand.nextInt( 220 );
-            g = rand.nextInt( 220 );
-            b = rand.nextInt( 220 );
+            double[] mcolor = new double[]{b, g, r};
+            r = rand.nextInt(220);
+            g = rand.nextInt(220);
+            b = rand.nextInt(220);
 
-            for ( Point point : line.points ) {
-                img.put( point.y, point.x, mcolor );
+            for (Point point : line.points) {
+                img.put(point.y, point.x, mcolor);
             }
-            DataCollector.INSTANCE.addtoHistory( "before interpolate", img );
+            DataCollector.INSTANCE.addtoHistory("before interpolate", img);
         }
 
-        for ( Line line : lines ) {
-            if ( line.points.size() <= 2 )
+        for (Line line : lines) {
+            if (line.points.size() <= 2)
                 continue;
 
 
             Line x = new Line();
             Line y = new Line();
             int i = 0;
-            for ( Point p : line.points ) {
-                x.add( new Point( i, p.x ) );
-                y.add( new Point( i, p.y ) );
+            for (Point p : line.points) {
+                x.add(new Point(i, p.x));
+                y.add(new Point(i, p.y));
                 i++;
             }
 
-            regression.extractPointsFormLine( x );
-            Line lrx = regression.regress();
+            optimizer.extractPointsFormLine(x);
+            Line lrx = optimizer.optimize();
 
-            regression.extractPointsFormLine( y );
-            Line lry = regression.regress();
+            optimizer.extractPointsFormLine(y);
+            Line lry = optimizer.optimize();
 
-            interpolacion.extractPointsFormLine( x );
+            interpolacion.extractPointsFormLine(x);
             Line lix = interpolacion.interpolate();
-            interpolacion.extractPointsFormLine( y );
+            interpolacion.extractPointsFormLine(y);
             Line liy = interpolacion.interpolate();
 
-            double[] mcolor = new double[]{ b, g, r };
-            r = rand.nextInt( 220 );
-            g = rand.nextInt( 220 );
-            b = rand.nextInt( 220 );
+            double[] mcolor = new double[]{b, g, r};
+            r = rand.nextInt(220);
+            g = rand.nextInt(220);
+            b = rand.nextInt(220);
 
-            for ( Point p : line.points ) {
-                int id = line.points.indexOf( p );
-                p.x = lrx.points.get( id ).y;
-                p.y = lry.points.get( id ).y;
+            for (Point p : line.points) {
+                int id = line.points.indexOf(p);
+                p.x = lrx.points.get(id).y;
+                p.y = lry.points.get(id).y;
 
-                im_regr.put( p.y, p.x, mcolor );
+                im_regr.put(p.y, p.x, mcolor);
             }
 
-            for ( Point p : line.points ) {
-                int id = line.points.indexOf( p );
-                p.x = lix.points.get( id ).y;
-                p.y = liy.points.get( id ).y;
+            for (Point p : line.points) {
+                int id = line.points.indexOf(p);
+                p.x = lix.points.get(id).y;
+                p.y = liy.points.get(id).y;
 
-                im_inter.put( p.y, p.x, mcolor );
+                im_inter.put(p.y, p.x, mcolor);
             }
 
             //interpolacion.run();
             //break;
         }
 
-        for ( Point p : epoints ) {
-            if ( p.isCrossroad ) {
-                img.put( p.y, p.x, new double[]{ 255, 255, 255 } );
-                im_inter.put( p.y, p.x, new double[]{ 255, 255, 255 } );
-                im_regr.put( p.y, p.x, new double[]{ 255, 255, 255 } );
+        for (Point p : epoints) {
+            if (p.isCrossroad) {
+                img.put(p.y, p.x, new double[]{255, 255, 255});
+                im_inter.put(p.y, p.x, new double[]{255, 255, 255});
+                im_regr.put(p.y, p.x, new double[]{255, 255, 255});
             }
         }
-        DataCollector.INSTANCE.addtoHistory( "before interpolate", img );
-        DataCollector.INSTANCE.addtoHistory( "after interpolate", im_inter );
-        DataCollector.INSTANCE.addtoHistory( "after regression", im_regr );
+        DataCollector.INSTANCE.addtoHistory("before interpolate", img);
+        DataCollector.INSTANCE.addtoHistory("after interpolate", im_inter);
+        DataCollector.INSTANCE.addtoHistory("after regression", im_regr);
 
         result = im_regr;
 
-        Linox.getInstance().getStatusBar().setProgress( title, 100, 100 );
+        Linox.getInstance().getStatusBar().setProgress(title, 100, 100);
 
-        if ( pluginListener != null ) {
+        if (pluginListener != null) {
             pluginListener.addImageTab();
             pluginListener.finishPlugin();
         }
---
+++ src/plugins/approximation/Optimizer.java
@@ -0,0 +1,122 @@
+package plugins.approximation;
+
+import entities.Line;
+import entities.Point;
+import org.apache.commons.math3.analysis.ParametricUnivariateFunction;
+import org.apache.commons.math3.fitting.CurveFitter;
+import org.apache.commons.math3.optim.nonlinear.vector.jacobian.LevenbergMarquardtOptimizer;
+import org.apache.commons.math3.stat.StatUtils;
+import org.math.plot.Plot2DPanel;
+
+public class Optimizer {
+    double[] xArr, yArr;
+    Plot2DPanel plot;
+    LevenbergMarquardtOptimizer optimizer;
+    CurveFitter fitter;
+
+    public Line optimize() {
+        Line line = new Line();
+
+        for (int i = 0; i < xArr.length; i++) {
+            fitter.addObservedPoint(xArr[i], yArr[i]);
+        }
+
+        ParabolaFunction sif = new ParabolaFunction(); // Class provided below
+        double[] initialguess = new double[3];
+        initialguess[0] = 1.0d;
+        initialguess[1] = 2.5d;
+        initialguess[2] = 1.0d;
+        double[] bestCoefficients = fitter.fit(sif, initialguess);
+
+
+        int n = (int) (Math.abs(StatUtils.max(xArr) - StatUtils.min(xArr)));
+        double[] xc = new double[n + 1];
+        double[] yc = new double[n + 1];
+        double xi = StatUtils.min(xArr);
+        for (int i = 0; i < xc.length; i++) {
+            xc[i] = xi + i;
+            yc[i] = sif.value(xc[i], bestCoefficients);
+            line.add(new Point((int) xc[i], (int) yc[i]));
+        }
+        return line;
+    }
+
+    public void extractPointsFormLine(Line line) {
+        xArr = new double[line.points.size()];
+        yArr = new double[line.points.size()];
+        optimizer = new LevenbergMarquardtOptimizer();
+        fitter = new CurveFitter(optimizer);
+        plot = new Plot2DPanel();
+        int i = 0;
+        for (Point point : line.points) {
+            xArr[i] = point.x;
+            yArr[i] = point.y;
+            i++;
+
+        }
+    }
+
+    /**
+     * This is my implementation of ParametricRealFunction
+     * Implements y = ax^-1 + b for use with an Apache CurveFitter implementation
+     */
+    /*private class SimpleInverseFunction2 implements ParametricUnivariateFunction
+    {
+        public double value(double x, double[] doubles) throws FunctionEvaluationException
+        {
+            //y = ax^-1 + b
+            //"double[] must include at least 1 but not more than 2 coefficients."
+            if(doubles == null || doubles.length ==0 || doubles.length > 2) throw new FunctionEvaluationException(doubles);
+            double a = doubles[0];
+            double b = 0;
+            if(doubles.length >= 2) b = doubles[1];
+            return a * Math.pow(x, -1d) + b;
+        }
+        public double[] gradient(double x, double[] doubles) throws FunctionEvaluationException
+        {
+            //derivative: -ax^-2
+            //"double[] must include at least 1 but not more than 2 coefficients."
+            if(doubles == null || doubles.length ==0 || doubles.length > 2) throw new FunctionEvaluationException(doubles);
+            double a = doubles[0];
+            double b = 0;
+            if(doubles.length >= 2) b = doubles[1];
+            double derivative = -a * Math.pow(x, -2d);
+            double[]gradientVector = new double[1];
+            gradientVector[0] = derivative;
+            return gradientVector;
+        }
+    }*/
+
+    private static class SimpleInverseFunction implements ParametricUnivariateFunction {
+
+        public double value(double x, double[] parameters) {
+            return parameters[0] / x + (parameters.length < 2 ? 0 : parameters[1]);
+        }
+
+        public double[] gradient(double x, double[] doubles) {
+            double[] gradientVector = new double[doubles.length];
+            gradientVector[0] = 1 / x;
+            if (doubles.length >= 2) {
+                gradientVector[1] = 1;
+            }
+            return gradientVector;
+        }
+    }
+
+    private static class ParabolaFunction implements ParametricUnivariateFunction {
+        @Override
+        public double value(double x, double[] parameters) {
+            return parameters[0] * x * x + parameters[1] * x + parameters[2];
+        }
+
+        @Override
+        public double[] gradient(double x, double[] doubles) {
+            double[] gradientVector = new double[doubles.length];
+            gradientVector[0] = x * x;
+            gradientVector[1] = x;
+            gradientVector[2] = 1;
+            return gradientVector;
+        }
+    }
+
+}
